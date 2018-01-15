c22bae4eb8c613c3492b05d2d0c51297b14afe6a
LizAharonian <lizaharonian@gmail.com>
2018-01-15 15:41:06 +0200

    cancel call to kernal func inserted inside smooth

--- myfunction.c
+++ myfunction.c
@@ -45,14 +45,14 @@ static void assign_sum_to_pixel(pixel *current_pixel, pixel_sum sum, int kernelS
 
 /*
 * sum_pixels_by_weight - Sums pixel values, scaled by given number
-*/
+*//*
 static void sum_pixels_by_weight(pixel_sum *sum, pixel p, int weight) {
     sum->red += ((int) p.red) * weight;
     sum->green += ((int) p.green) * weight;
     sum->blue += ((int) p.blue) * weight;
     sum->num++;
     return;
-}
+}*/
 
 /*
  *  Applies kernel for pixel at (i,j)
@@ -104,7 +104,7 @@ static pixel applyKernel(int dim, int i, int j, pixel *src, int kernelSize, int 
     }
 
     // assign kernel's result to pixel at [i,j]
-   // assign_sum_to_pixel(&current_pixel, sum, kernelScale);
+    // assign_sum_to_pixel(&current_pixel, sum, kernelScale);
     //todo: remove call to func assign_sum_to_pixel
 
     // divide by kernel's weight
@@ -131,7 +131,76 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
     dimMinusHalfKernal = dim - halfK;
     for (i = halfK ; i < dimMinusHalfKernal; i++) {
         for (j =  halfK ; j < dimMinusHalfKernal ; j++) {
-            dst[calcIndex(i, j, dim)] = applyKernel(dim, i, j, src, kernelSize, kernel, kernelScale);
+
+            //*************
+
+
+
+            int ii, jj;
+            int currRow, currCol;
+            pixel_sum sum;
+            pixel current_pixel;
+
+            sum.red = sum.green = sum.blue = 0;
+            sum.num = 0;
+
+            //initialize_pixel_sum(&sum);
+
+            for(ii = max(i-1, 0); ii <= min(i+1, dim-1); ii++) {
+                for(jj = max(j-1, 0); jj <= min(j+1, dim-1); jj++) {
+
+                    int kRow, kCol;
+
+                    // compute row index in kernel
+                    if (ii < i) {
+                        kRow = 0;
+                    } else if (ii > i) {
+                        kRow = 2;
+                    } else {
+                        kRow = 1;
+                    }
+
+                    // compute column index in kernel
+                    if (jj < j) {
+                        kCol = 0;
+                    } else if (jj > j) {
+                        kCol = 2;
+                    } else {
+                        kCol = 1;
+                    }
+
+                    // apply kernel on pixel at [ii,jj]
+                    //sum_pixels_by_weight(&sum, src[calcIndex(ii, jj, dim)], kernel[kRow][kCol]);
+                    pixel p = src[calcIndex(ii, jj, dim)];
+                    int weight = kernel[kRow][kCol];
+                    sum.red += ((int) p.red) * weight;
+                    sum.green += ((int) p.green) * weight;
+                    sum.blue += ((int) p.blue) * weight;
+                    sum.num++;
+                }
+            }
+
+            // assign kernel's result to pixel at [i,j]
+            // assign_sum_to_pixel(&current_pixel, sum, kernelScale);
+            //todo: remove call to func assign_sum_to_pixel
+
+            // divide by kernel's weight
+            sum.red = sum.red / kernelScale;
+            sum.green = sum.green / kernelScale;
+            sum.blue = sum.blue / kernelScale;
+
+            // truncate each pixel's color values to match the range [0,255]
+            current_pixel.red = (unsigned char) (min(max(sum.red, 0), 255));
+            current_pixel.green = (unsigned char) (min(max(sum.green, 0), 255));
+            current_pixel.blue = (unsigned char) (min(max(sum.blue, 0), 255));
+            //return current_pixel;
+
+
+
+            //****
+
+
+            dst[calcIndex(i, j, dim)] = current_pixel;//applyKernel(dim, i, j, src, kernelSize, kernel, kernelScale);
         }
     }
 }
