00ec4210dd35259a1bb0968bedae184a564e57f3
LizAharonian <lizaharonian@gmail.com>
2018-01-15 16:53:55 +0200

    remove wheight because its 1

--- myfunction.c
+++ myfunction.c
@@ -137,7 +137,7 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
 
 
             int ii, jj;
-            int currRow, currCol;
+          //  int currRow, currCol;
             pixel_sum sum;
             pixel current_pixel;
 
@@ -199,11 +199,12 @@ void smooth(int dim, pixel *src, pixel *dst, int kernelSize, int kernel[kernelSi
 
             //****
 
-
+            //todo: remove call to applyKernel
             dst[calcIndex(i, j, dim)] = current_pixel;//applyKernel(dim, i, j, src, kernelSize, kernel, kernelScale);
         }
     }
 }
+/*
 
 void charsToPixels(Image *charsImg, pixel* pixels) {
 
@@ -244,9 +245,10 @@ void copyPixels(pixel* src, pixel* dst) {
         }
     }
 }
+*/
 
 
-void doConvolution(Image *image, int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale) {
+void doConvolutionBlur(Image *image, int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale) {
 /*
 
     pixel* pixelsImg = malloc(m*n*sizeof(pixel));
@@ -255,17 +257,131 @@ void doConvolution(Image *image, int kernelSize, int kernel[kernelSize][kernelSi
     charsToPixels(image, pixelsImg);
     copyPixels(pixelsImg, backupOrg);
 */
-    pixel* pixelsImg = (pixel*) image->data;
-    pixel* backupOrg = malloc(m*n*sizeof(pixel));
-    memcpy(backupOrg, pixelsImg , m*m*3);
+    //todo: delete these stupid conversions.
+    int x = m*n*3;
+    pixel* pixelsImg = (pixel*)image->data;
+    pixel* backupOrg = malloc(x);
+    memcpy(backupOrg, pixelsImg , x);
 
-    smooth(m, backupOrg, pixelsImg, kernelSize, kernel, kernelScale);
+   // smooth(m, backupOrg, pixelsImg, kernelSize, kernel, kernelScale);
+    //todo: get ride of calling to smooth func
+
+    //todo: get ride of repptive calculations
+    int i, j, halfK, dimMinusHalfKernal;
+    halfK = kernelSize / 2;
+    dimMinusHalfKernal = m - halfK;
+    for (i = halfK ; i < dimMinusHalfKernal; i++) {
+        for (j =  halfK ; j < dimMinusHalfKernal ; j++) {
+
+            //*************
+
+
+
+            int ii, jj;
+            int currRow, currCol;
+            pixel_sum sum;
+            pixel current_pixel;
+
+            sum.red = sum.green = sum.blue = 0;
+            sum.num = 0;
+
+            //initialize_pixel_sum(&sum);
+
+            for(ii = max(i-1, 0); ii <= min(i+1, m-1); ii++) {
+                for(jj = max(j-1, 0); jj <= min(j+1, m-1); jj++) {
+
+                    /*int kRow, kCol;
+
+                    // compute row index in kernel
+                    if (ii < i) {
+                        kRow = 0;
+                    } else if (ii > i) {
+                        kRow = 2;
+                    } else {
+                        kRow = 1;
+                    }
+
+                    // compute column index in kernel
+                    if (jj < j) {
+                        kCol = 0;
+                    } else if (jj > j) {
+                        kCol = 2;
+                    } else {
+                        kCol = 1;
+                    }*/
+
+                    // apply kernel on pixel at [ii,jj]
+                    //sum_pixels_by_weight(&sum, src[calcIndex(ii, jj, dim)], kernel[kRow][kCol]);
+                    pixel p = backupOrg[calcIndex(ii, jj, m)];
+                    //int weight = 1;//kernel[kRow][kCol];
+                    sum.red += ((int) p.red);
+                    sum.green += ((int) p.green);
+                    sum.blue += ((int) p.blue);
+                    sum.num++;
+                }
+            }
+
+            // assign kernel's result to pixel at [i,j]
+            // assign_sum_to_pixel(&current_pixel, sum, kernelScale);
+            //todo: remove call to func assign_sum_to_pixel
+
+            // divide by kernel's weight
+            sum.red = sum.red / kernelScale;
+            sum.green = sum.green / kernelScale;
+            sum.blue = sum.blue / kernelScale;
+
+            // truncate each pixel's color values to match the range [0,255]
+            current_pixel.red = (unsigned char) (min(max(sum.red, 0), 255));
+            current_pixel.green = (unsigned char) (min(max(sum.green, 0), 255));
+            current_pixel.blue = (unsigned char) (min(max(sum.blue, 0), 255));
+            //return current_pixel;
+
+
+
+            //****
+
+            //todo: remove call to applyKernel
+            pixelsImg[calcIndex(i, j, m)] = current_pixel;//applyKernel(dim, i, j, src, kernelSize, kernel, kernelScale);
+        }
+    }
+
+
+
+
 
    // pixelsToChars(pixelsImg, image);
 
     //free(pixelsImg);
     free(backupOrg);
 }
+
+
+
+
+
+void doConvolutionSharpen(Image *image, int kernelSize, int kernel[kernelSize][kernelSize], int kernelScale) {
+/*
+
+    pixel* pixelsImg = malloc(m*n*sizeof(pixel));
+    pixel* backupOrg = malloc(m*n*sizeof(pixel));
+
+    charsToPixels(image, pixelsImg);
+    copyPixels(pixelsImg, backupOrg);
+*/
+    //todo: delete these stupid conversions.
+    int x = m*n*3;
+    pixel* pixelsImg = (pixel*)image->data;
+    pixel* backupOrg = malloc(x);
+    memcpy(backupOrg, pixelsImg , x);
+
+    smooth(m, backupOrg, pixelsImg, kernelSize, kernel, kernelScale);
+
+    // pixelsToChars(pixelsImg, image);
+
+    //free(pixelsImg);
+    free(backupOrg);
+}
+
 
 void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sharpRsltImgName) {
 
@@ -284,7 +400,7 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
     int sharpKernel[3][3] = {{-1,-1,-1},{-1,9,-1},{-1,-1,-1}};
 
     // blur image
-    doConvolution(image, 3, blurKernel, 9);
+    doConvolutionBlur(image, 3, blurKernel, 9);
 
     // write result image to file
     writeBMP(image, srcImgpName, blurRsltImgName);
@@ -292,7 +408,7 @@ void myfunction(Image *image, char* srcImgpName, char* blurRsltImgName, char* sh
     //smooth(m, backupOrg, pixelsImg, kernelSize, sharpKernel, 1);
 
     // sharpen the resulting image
-    doConvolution(image, 3, sharpKernel, 1);
+    doConvolutionSharpen(image, 3, sharpKernel, 1);
 
     // write result image to file
     writeBMP(image, srcImgpName, sharpRsltImgName);
